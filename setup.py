a0d0c948b0742dbaa78ecdb1ec09dd7f3b2aa483
cxan96 <c.p.xanten@gmail.com>
2021-03-17 15:22:52 +0100

    use whitenoise to serve static files

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=["flask", "gunicorn"],
+    install_requires=["flask", "whitenoise", "gunicorn"],
 )
--- strong_but_simple_passwords/__init__.py
+++ strong_but_simple_passwords/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from whitenoise import WhiteNoise
+from pathlib import Path
 from .config import get_config_from_env_vars
 from . import views
 
@@ -12,5 +14,9 @@ def create_app(config=None):
     app.config.from_object(config)
 
     app.add_url_rule("/", "index", view_func=views.index, methods=("GET", "POST"))
+
+    # use whitenoise to serve static files
+    static_root = Path(__file__).parent / "static/"
+    app.wsgi_app = WhiteNoise(app.wsgi_app, root=static_root, prefix="static/")
 
     return app
